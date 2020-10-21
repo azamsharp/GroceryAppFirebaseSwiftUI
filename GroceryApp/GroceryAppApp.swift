3af126cc1f25e9910eb79cf8f1fad8a8559fa1c8
Mohammad Azam <azamsharp@gmail.com>
2020-10-21 08:03:51 -0500

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Mohammad Azam on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
